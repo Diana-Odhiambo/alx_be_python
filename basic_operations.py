7a6d92c91e41fff90196b9602d6cf971803a87de
Diana-Odhiambo <nthenyadev@gmail.com>
2025-02-04 05:37:40 +0300

    first commit

---
+++ basic_operations.py
@@ -0,0 +1,13 @@
+# This program performs basic arithmetic operations
+num1 = int(input("Enter first number: ")) 
+num2 = int(input("Enter second number: "))
+
+# Performing basic calculations
+sum_result = num1 + num2
+sub_result = num1 - num2
+mul_result = num1 * num2
+
+# Printing the results
+print("The sum is:", sum_result)
+print("The difference is:", sub_result)
+print("The product is:", mul_result)
