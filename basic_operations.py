31a6c3a781926acdc8e87d7341beaafe39e072ee
Diana-Odhiambo <nthenyadev@gmail.com>
2025-02-05 04:25:50 +0300

    makes changes to variable name to pass the tests

--- basic_operations.py
+++ basic_operations.py
@@ -1,13 +1,14 @@
 # This program performs basic arithmetic operations
-num1 = int(input("Enter first number: ")) 
-num2 = int(input("Enter second number: "))
+number1 = int(input("Enter first number: ")) 
+number2 = int(input("Enter second number: "))
 
 # Performing basic calculations
-sum_result = num1 + num2
-sub_result = num1 - num2
-mul_result = num1 * num2
+sum_result = number1 + number2
+sub_result = number1 - number2
+mul_result = number1 * number2
 
 # Printing the results
+
 print("The sum is:", sum_result)
 print("The difference is:", sub_result)
 print("The product is:", mul_result)
