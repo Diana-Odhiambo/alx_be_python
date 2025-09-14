c089e0bdbce57b5d1b67a134c7888246079d5a2e
Diana-Odhiambo <nthenyadev@gmail.com>
2025-09-14 20:40:21 +0300

    Initiall commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,16 @@
+#Assignment of variables
+number1 = 10
+number2 = 5
+
+#Performing Arithmetic operations.
+Addition = number1 + number2
+Subtraction = number1 - number2
+Multiplication = number1 * number2
+
+#Result
+print("Addition of", number1, "and", number2, "is", Addition)
+print("Subtraction of", number1, "and", number2, "is", Subtraction)
+print("Multiplication of", number1, "and", number2, "is", Multiplication)
+
+
+
