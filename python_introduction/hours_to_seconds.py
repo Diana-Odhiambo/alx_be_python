74c24bb02cd2b7aff368144d3c27d0d62df918e0
Diana-Odhiambo <nthenyadev@gmail.com>
2025-09-14 21:36:56 +0300

    Program converts hours to seconds.

---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,11 @@
+# program converts a specific number of hours into seconds
+hours = 2
+
+#Define the constant value for seconds in an hour
+seconds_per_hour = 3600
+
+# Calculates the number of seconds in given hours
+seconds_result = hours * seconds_per_hour
+
+# printing the results
+print(f"{hours} hour(s) is {seconds_result} seconds")
