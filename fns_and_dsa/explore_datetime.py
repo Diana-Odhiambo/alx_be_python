762d91cbe26bf177909cb1ffc4b0f4d7e5725ad8
Diana-Odhiambo <nthenyadev@gmail.com>
2025-09-28 20:59:49 +0300

    Fix explore_datetime.py: add timedelta, display_current_datetime, and calculate_future_date implementations

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,11 +1,23 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 def display_current_datetime():
-    
-    current_datetime = datetime.now()
-    
-    formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
-    
-    print("Current Date and Time:", formatted_datetime)
-    
-display_current_datetime()
+    current_date = datetime.now()
+    formatted_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    print("Current date and time:", formatted_date)
+    return formatted_date
+
+def calculate_future_date(days):
+    current_date = datetime.now()
+    future_date = current_date + timedelta(days=days)
+    formatted_future = future_date.strftime("%Y-%m-%d")
+    print("Future date:", formatted_future)
+    return formatted_future
+
+if __name__ == "__main__":
+    # Part 1
+    display_current_datetime()
+
+    # Part 2
+    days = int(input("Enter the number of days to add to the current date: "))
+    calculate_future_date(days)
+
